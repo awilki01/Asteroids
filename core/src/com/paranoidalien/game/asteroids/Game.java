39abe309fbba85fbc8da39aba698aee09dc7d0f9
awilki01 <adam.wilkins@mygrande.net>
2014-10-18 16:34:23 -0500

    Part3

--- core/src/com/paranoidalien/game/asteroids/Game.java
+++ core/src/com/paranoidalien/game/asteroids/Game.java
@@ -8,6 +8,8 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.paranoidalien.game.managers.GameInputProcessor;
+import com.paranoidalien.game.managers.GameKeys;
 
 public class Game implements ApplicationListener {
 
@@ -19,13 +21,14 @@ public class Game implements ApplicationListener {
 
     @Override
     public void create() {
-
         WIDTH = Gdx.graphics.getWidth();
         HEIGHT = Gdx.graphics.getHeight();
 
         cam = new OrthographicCamera(WIDTH, HEIGHT);
         cam.translate(WIDTH/2, HEIGHT/2);
         cam.update();
+
+        Gdx.input.setInputProcessor(new GameInputProcessor());
     }
 
     @Override
@@ -34,6 +37,14 @@ public class Game implements ApplicationListener {
         // Clear screen to black
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+
+        if(GameKeys.isPressed(GameKeys.SPACE)) {
+            System.out.println("SPACE");
+        }
+
+        GameKeys.update();
 
     }
 
