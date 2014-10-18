fb3af3745b6884d59293060601c14ab77bbd1721
awilki01 <adam.wilkins@mygrande.net>
2014-10-18 10:13:02 -0500

    Part 1

--- desktop/src/com/paranoidalien/game/asteroids/desktop/DesktopLauncher.java
+++ desktop/src/com/paranoidalien/game/asteroids/desktop/DesktopLauncher.java
@@ -1,6 +1,3 @@
-// This is MASTER
-
-
 package com.paranoidalien.game.asteroids.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
@@ -10,6 +7,14 @@ import com.paranoidalien.game.asteroids.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Game(), config);
+
+        config.title = "Asteroids";
+        config.width = 500;
+        config.height = 400;
+        config.useGL30 = false;
+        config.resizable = false;
+
+        new LwjglApplication(new Game(), config);
+
 	}
 }
